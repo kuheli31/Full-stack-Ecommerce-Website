7eb0700bfa01902bf6252c797a2d25e9de75c4a4
kuheli31 <133209155+kuheli31@users.noreply.github.com>
2025-04-20 13:48:03 +0530

    Files added

--- src/customer/components/HomeCarousel/MainCarouselData.js
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -1,7 +1,15 @@
 export const MainCarouselData = [
     {
-        image:
-        path:"/women"
+        image:"https://th.bing.com/th/id/OPAC.lQw773uZLEtKeA474C474?w=592&h=550&o=5&dpr=1.3&pid=21.1",
+        path:"/women/clothing/lehenga_choli"
+    },
+    {
+        image:"https://image.hm.com/assets/hm/b6/f5/b6f5ea411a62a526735038168ca6fdb810fc9da3.jpg?imwidth=1260",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://th.bing.com/th/id/OPAC.NfQ3sgvE5iEp6w474C474?w=160&h=220&c=17&dpr=1.3&pid=21.1",
+        path:"/women/clothing/women_saree"
     }
 ]
 
